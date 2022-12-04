cc12fee5bcbcdfc14e64dfb683d7ca8349bf427a
Yerin <myheaven_b@naver.com>
2022-12-04 16:48:38 +0900

    refactor: 문자열 const 값으로 변경

--- src/main/kotlin/vendingmachine/Exceptions.kt
+++ src/main/kotlin/vendingmachine/Exceptions.kt
@@ -4,12 +4,14 @@ class Exceptions {
     fun validateAdminInput(input: String) {
         val money = input.toIntOrNull()
         require(money != null) { throw IllegalArgumentException(WRONG_INPUT_TYPE_MESSAGE) }
-        require(money >= 0) { throw IllegalArgumentException("투입금액은 0원 이상부터 가능합니다.") }
-        require(money % 10 == 0) { throw IllegalArgumentException("투입금액은 최소 10원단위가 가능합니다.") }
+        require(money >= 0) { throw IllegalArgumentException(WRONG_INPUT_RANGE_MESSAGE) }
+        require(money % 10 == 0) { throw IllegalArgumentException(WRONG_INPUT_REST_MESSAGE) }
     }
 
     companion object {
         const val WRONG_INPUT_TYPE_MESSAGE = "투입금액은 숫자만 입력가능합니다."
+        const val WRONG_INPUT_RANGE_MESSAGE = "투입금액은 0원 이상부터 가능합니다."
+        const val WRONG_INPUT_REST_MESSAGE = "투입금액은 최소 10원단위가 가능합니다."
     }
 
 }
