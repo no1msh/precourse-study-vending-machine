5318295ffb26df66d2ef4411aa9ea6d58015f591
meltnia <themtale@gmail.com>
2022-12-05 21:57:28 +0900

    feat: VendingManager 뼈대 작성

--- src/main/kotlin/vendingmachine/domain/VendingManager.kt
+++ src/main/kotlin/vendingmachine/domain/VendingManager.kt
@@ -1,9 +1,30 @@
 package vendingmachine.domain
 
+import vendingmachine.model.CoinList
+
 class VendingManager {
 
+    val coins: CoinList = CoinList()
     fun start() {
+        getMoney()
+        getItems()
+        doShoping()
+        giveChange()
+    }
+
+    fun getMoney() {
 
     }
 
+    fun getItems() {
+
+    }
+
+    fun doShoping() {
+
+    }
+
+    fun giveChange() {
+
+    }
 }
