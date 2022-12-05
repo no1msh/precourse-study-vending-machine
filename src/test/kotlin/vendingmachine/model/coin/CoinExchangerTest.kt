f56395d40721d7246b2b135541d6b18e676ec80f
Dev-Joco <csr8367@hanmail.net>
2022-12-05 22:34:07 +0900

    test(model): CoinExchanger.exchange() 기능 테스트 작성

---
+++ src/test/kotlin/vendingmachine/model/coin/CoinExchangerTest.kt
@@ -0,0 +1,52 @@
+package vendingmachine.model.coin
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.assertThrows
+import vendingmachine.model.random.RandomGenerator
+
+internal class CoinExchangerTest {
+
+    @Test
+    fun `동전교환테스트_성공1`() {
+        val generator = NumberGeneratorForTest(listOf(10, 50, 100, 500))
+        val exchanger = CoinExchanger(generator)
+
+        val coinCounts = exchanger.exchange(660)
+        assertThat(coinCounts[Coin.COIN_10]).isEqualTo(1)
+        assertThat(coinCounts[Coin.COIN_50]).isEqualTo(1)
+        assertThat(coinCounts[Coin.COIN_100]).isEqualTo(1)
+        assertThat(coinCounts[Coin.COIN_500]).isEqualTo(1)
+    }
+
+    @Test
+    fun `동전교환테스트_성공2`() {
+        val generator = NumberGeneratorForTest(listOf(10, 50, 100, 500))
+        val exchanger = CoinExchanger(generator)
+
+        val coinCounts = exchanger.exchange(825)
+        assertThat(coinCounts[Coin.COIN_10]).isEqualTo(2)
+        assertThat(coinCounts[Coin.COIN_50]).isEqualTo(2)
+        assertThat(coinCounts[Coin.COIN_100]).isEqualTo(2)
+        assertThat(coinCounts[Coin.COIN_500]).isEqualTo(1)
+    }
+
+    @Test
+    fun `동전교환테스트_예외`() {
+        val generator = NumberGeneratorForTest(listOf(11))
+        val exchanger = CoinExchanger(generator)
+
+        assertThrows<NullPointerException> {
+            exchanger.exchange(1000)
+        }
+    }
+
+    class NumberGeneratorForTest(private val numbers: List<Int>) : RandomGenerator {
+
+        private var index = -1
+
+        override fun generate(): Int = numbers[++index % numbers.size]
+    }
+}
