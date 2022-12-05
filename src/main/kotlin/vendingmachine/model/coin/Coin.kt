bda26a765d2f55131b7f85a9a92352ff77de03c7
Dev-Joco <csr8367@hanmail.net>
2022-12-05 21:30:48 +0900

    feat(model): Coin.companion.get() 함수 구현

--- src/main/kotlin/vendingmachine/model/coin/Coin.kt
+++ src/main/kotlin/vendingmachine/model/coin/Coin.kt
@@ -1,10 +1,17 @@
 package vendingmachine.model.coin
 
-enum class Coin(private val amount: Int) {
+enum class Coin(private val amount: Int) : Comparable<Coin> {
     COIN_500(500),
     COIN_100(100),
     COIN_50(50),
     COIN_10(10);
 
-    // 추가 기능 구현
+
+
+    companion object {
+
+        private val amounts: Map<Int, Coin> = values().associateBy { it.amount }
+
+        operator fun get(amount: Int): Coin = amounts[amount]!!
+    }
 }
