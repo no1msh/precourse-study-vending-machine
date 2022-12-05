359fdf0a72957a994cedf6df24a3ba4812a3c8ce
meltnia <themtale@gmail.com>
2022-12-05 22:55:07 +0900

    feat: 자판기 입력부 일부 구현

--- src/main/kotlin/vendingmachine/view/InputView.kt
+++ src/main/kotlin/vendingmachine/view/InputView.kt
@@ -6,9 +6,24 @@ import camp.nextstep.edu.missionutils.Console
 class InputView {
 
     fun giveMeCoin(): Int {
-        val money = Console.readLine()
-
-
+        var check = false
+        var money = ""
+        while (!check) {
+            money = Console.readLine()
+            check = errorCheckCoin(money)
+        }
         return money.toInt()
+    }
+
+
+    private fun errorCheckCoin(money: String): Boolean {
+        return try {
+            VendingException().isItNumber(money)
+            VendingException().isItHaveRemainder(money.toInt())
+            true
+        } catch (error: IllegalArgumentException) {
+            println(error.message)
+            false
+        }
     }
 }
--- src/main/kotlin/vendingmachine/view/VendingException.kt
+++ src/main/kotlin/vendingmachine/view/VendingException.kt
@@ -5,7 +5,7 @@ const val NOT_NUMBER = "금액은 숫자여야합니다."
 const val CAN_NOT_DIVIDE_10 = "10으로 나눴을 때 나머지가 없어야합니다."
 const val IT_IS_UNDER_100 = "상품의 가격은 100 이상이여야 합니다."
 
-class Exception {
+class VendingException {
     fun isItNumber(number: String) {
         require(number.toIntOrNull() != null) { "$ERROR $NOT_NUMBER" }
     }
