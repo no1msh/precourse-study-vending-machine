3cb097e759be7fc96a475b456f94043dd20394b3
Dev-Joco <csr8367@hanmail.net>
2022-12-05 21:33:21 +0900

    feat(model): 동전 교환 함수 CoinExchanger.exchange() 기능 구현

--- src/main/kotlin/vendingmachine/model/coin/CoinExchanger.kt
+++ src/main/kotlin/vendingmachine/model/coin/CoinExchanger.kt
@@ -5,5 +5,20 @@ import java.util.*
 
 class CoinExchanger(private val generator: RandomGenerator) {
 
+    fun exchange(money: Int): EnumMap<Coin, Int> {
+        val coinCounts = EnumMap<Coin, Int>(Coin::class.java)
+        var deposit = money
 
+        while (Coin.MIN_AMOUNT <= deposit) {
+            val amount = generator.generate()
+            if (deposit < amount) {
+                continue
+            }
+
+            deposit -= amount
+            coinCounts[Coin[amount]]!!.plus(1)
+        }
+
+        return coinCounts
+    }
 }
