2215c5f30b4a9e2bd175c82d160ccfc287d45ef9
Dev-Joco <csr8367@hanmail.net>
2022-12-05 21:31:52 +0900

    feat(model): Coin 최대/최소 상수 정의

--- src/main/kotlin/vendingmachine/model/coin/Coin.kt
+++ src/main/kotlin/vendingmachine/model/coin/Coin.kt
@@ -6,9 +6,9 @@ enum class Coin(private val amount: Int) : Comparable<Coin> {
     COIN_50(50),
     COIN_10(10);
 
-
-
     companion object {
+        val MAX_AMOUNT = values().maxOf { it.amount }
+        val MIN_AMOUNT = values().minOf { it.amount }
 
         private val amounts: Map<Int, Coin> = values().associateBy { it.amount }
 
