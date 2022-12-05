c0040434031fbbf9365384341c99d72222534455
meltnia <themtale@gmail.com>
2022-12-05 23:01:22 +0900

    test: 코인 생성 관련 테스트 작성

--- src/test/kotlin/vendingmachine/VendingTest.kt
+++ src/test/kotlin/vendingmachine/VendingTest.kt
@@ -1,6 +1,20 @@
 package vendingmachine
 
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInListTest
+import vendingmachine.model.CoinList
+
+
 class VendingTest {
 
+    fun `코인 생성 테스트`() {
+        assertRandomNumberInListTest(
+            {
+                val coins = CoinList()
+                coins.settingCoins(1150)
+                equals(mutableListOf(2, 1, 1, 0), coins.getcoins())
+
+            }, 500, 100, 500, 50
+        )
+    }
 
 }
