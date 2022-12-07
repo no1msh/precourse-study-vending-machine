135fa65e65c611ff8cac6425103134da6ede689e
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-12-07 17:54:32 +0900

    test(ProductTest): 테스트 코드 작성

---
+++ src/test/kotlin/vendingmachine/model/ProductTest.kt
@@ -0,0 +1,16 @@
+package vendingmachine.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class ProductTest {
+
+    @Test
+    fun `남은 돈 계산 테스트`() {
+        val money = 3000
+        val cider = Product("사이다", 1000, 20)
+        assertThat(cider.calculate(money)).isEqualTo(2000)
+    }
+}
