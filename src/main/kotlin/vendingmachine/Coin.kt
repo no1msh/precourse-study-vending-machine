b30525978c6e8a349dde46c3ee5a3222065ecf21
HwangSooHyeon <48857651+HwangSooHyeon@users.noreply.github.com>
2022-12-08 22:55:23 +0900

    feat(Coin): Coin 객체를 가져오는 함수 추가

--- src/main/kotlin/vendingmachine/Coin.kt
+++ src/main/kotlin/vendingmachine/Coin.kt
@@ -8,4 +8,16 @@ enum class Coin(private val amount: Int) {
 
     // 추가 기능 구현
     fun getAmount(): Int = amount
+
+    companion object {
+        fun getCoin(amount: Int): Coin? {
+            return when(amount) {
+                COIN_500.amount -> COIN_500
+                COIN_100.amount -> COIN_100
+                COIN_50.amount -> COIN_50
+                COIN_10.amount -> COIN_10
+                else -> null
+            }
+        }
+    }
 }
