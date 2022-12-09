7f49657edb687a9cd665e692b987e51a9201e6e2
no1msh <no1msh1217@gmail.com>
2022-12-10 01:37:06 +0900

    test: 자판기가 보유한 동전 출력 테스트 코드 수정

--- src/test/kotlin/vendingmachine/view/OutputViewTest.kt
+++ src/test/kotlin/vendingmachine/view/OutputViewTest.kt
@@ -4,11 +4,27 @@ import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
+import org.junit.jupiter.api.BeforeEach
+import vendingmachine.Coin
+import vendingmachine.controller.VendingMachineController
 import vendingmachine.main
 
-class OutputViewTest : NsTest(){
+class OutputViewTest : NsTest() {
+
+    private val haveCoin = mutableMapOf<Coin, Int>()
+
     @Test
-    fun `투입 금액을 제대로 출력하는가?`(inputMoney : String){
+    fun `자판기가 보유한 동전을 제대로 출력하는가?`() {
+        val inputMoney = "500원 - 1개\n100원 - 2개\n50원 - 3개\n10원 - 4개"
+        initHaveCoin()
+        OutputView.printVendingMachineCoins(haveCoin)
+        assertSimpleTest {
+            Assertions.assertThat(output()).isEqualTo(inputMoney)
+        }
+    }
+
+    @Test
+    fun `투입 금액을 제대로 출력하는가?`(inputMoney: String) {
 //        OutputView.printInputMoney()
         assertSimpleTest {
             Assertions.assertThat(output()).isEqualTo(inputMoney)
@@ -16,22 +32,23 @@ class OutputViewTest : NsTest(){
     }
 
     @Test
-    fun `자판기가 보유한 동전을 제대로 출력하는가?`(inputMoney : String){
-//        OutputView.printVendingMachineCoins()
-        assertSimpleTest {
-            Assertions.assertThat(output()).isEqualTo(inputMoney)
-        }
-    }
-
-    @Test
-    fun `반환할 잔돈을 제대로 출력하는가?`(inputMoney : String){
+    fun `반환할 잔돈을 제대로 출력하는가?`(inputMoney: String) {
 //        OutputView.printChange()
         assertSimpleTest {
             Assertions.assertThat(output()).isEqualTo(inputMoney)
         }
     }
+
     override fun runMain() {
         main()
     }
+
+    private fun initHaveCoin() {
+        haveCoin[Coin.COIN_500] = 1
+        haveCoin[Coin.COIN_100] = 2
+        haveCoin[Coin.COIN_50] = 3
+        haveCoin[Coin.COIN_10] = 4
+    }
+
 
 }
